8238eec8933231c133d236328f474174534e99f7
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-02-06 14:40:59 -0700

    set up homepage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,13 +5,45 @@ import PlayButton from "./PlayButton"
 export default function App() {
   return (
     <Wrapper className="App">
+      <ButtonContainer className="play-button">
       <PlayButton />
+      </ButtonContainer>
+      <Instruction><Spline scene="https://prod.spline.design/kimyBtkdtDt0rtFi/scene.splinecode" /></Instruction>
+      <SceneContainer className="spline-scene">
       <Spline scene="https://prod.spline.design/cwaI5825d30JGN9H/scene.splinecode" />
+      </SceneContainer>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 1000px;
+  overflow: hidden;
+`;
+
+const SceneContainer = styled.div`
+position:relative;
+background-size: cover;
+width: 100vw;
+height: 100vh;
+cursor: pointer;
+`;
+
+const ButtonContainer = styled.div`
+position: absolute;
+z-index: 1;
+height: 300px;
+width: 600px;
+margin-left: 25%;
+margin-top: 5%;
+align-item: center;
+`;
+
+const Instruction = styled.div`
+position: absolute;
+z-index: 1;
+margin-left: 25%;
+height: 50%;
+width: 50%;
+margin-top:25%;
 `;
