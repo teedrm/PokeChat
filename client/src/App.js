32fb9953d6ba9229d922074ad0d4d22f8c2eab76
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-02-05 20:05:35 -0700

    homepage set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Spline from "@splinetool/react-spline";
+import PlayButton from "/src/components/PlayButton"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper className="App">
+      <PlayButton />
+      <Spline scene="https://prod.spline.design/cwaI5825d30JGN9H/scene.splinecode" />
+    </Wrapper>
   );
 }
 
-export default App;
+const Wrapper = styled.div`
+  width: 100%;
+  height: 1000px;
+`;
---
+++ client/src/components/PlayButton.js
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/YKcnNintwgr6h7pn/scene.splinecode" />
+  );
+}
