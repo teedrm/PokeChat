acc427b4849d105ce5a62ffea411a68ab41cf7ff
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-02-07 23:09:07 -0700

    set up sign up page

---
+++ client/src/components/Sign up/Logo.js
@@ -0,0 +1,7 @@
+import Spline from "@splinetool/react-spline";
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/nUd3HC8nZqh5NogT/scene.splinecode" />
+  );
+}
