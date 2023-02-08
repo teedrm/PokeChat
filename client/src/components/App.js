063e4f09c00ec4ab784520a7f8765910588014a8
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-02-08 04:47:33 -0700

    setting up login form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import SignUp from "./Signup/main";
+import SignUp from "./Forms/main";
 import Login from "./Login";
 import Home from "./Home";
 import ChatRoom from "./ChatRoom";
