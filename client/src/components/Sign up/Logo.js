5716601fa41b3ce1d493be518f995dffd3e431fc
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-02-08 03:36:07 -0700

    setting routing for login and signup form

--- client/src/components/Sign up/Logo.js
+++
@@ -1,7 +0,0 @@
-import Spline from "@splinetool/react-spline";
-
-export default function App() {
-  return (
-    <Spline scene="https://prod.spline.design/nUd3HC8nZqh5NogT/scene.splinecode" />
-  );
-}
