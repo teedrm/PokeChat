3a12b7860a5cf8c70ec3e30376fd2c8f34324aae
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-02-08 03:33:53 -0700

    Fixing home page and setting up route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,53 +1,24 @@
-import styled from "styled-components";
-import Spline from "@splinetool/react-spline";
-import PlayButton from "./PlayButton"
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import SignUp from "./Signup/main";
+import Login from "./Login";
+import Home from "./Home";
 
 export default function App() {
 
 
   return (
-    <Wrapper className="App">
-      <ButtonContainer className="play-button">
-      <PlayButton />
+    <div>
+      <Router>
+        <Routes>
+          <Route path = "/" element={<Home/>} />
+          <Route path ="/signup" element={<SignUp/>} />
+          <Route path ="/login" element={<Login/>} />
+        </Routes>
+      </Router>
 
-      
-      </ButtonContainer>
-      <Instruction><Spline scene="https://prod.spline.design/kimyBtkdtDt0rtFi/scene.splinecode" /></Instruction>
-      <SceneContainer className="spline-scene">
-      <Spline scene="https://prod.spline.design/cwaI5825d30JGN9H/scene.splinecode" />
-      </SceneContainer>
-    </Wrapper>
+    </div>
   );
 }
 
-const Wrapper = styled.div`
-  width: 100%;
-  overflow: hidden;
-`;
-
-const SceneContainer = styled.div`
-position:relative;
-background-size: cover;
-width: 100vw;
-height: 100vh;
-cursor: pointer;
-`;
-
-const ButtonContainer = styled.div`
-position: absolute;
-z-index: 1;
-height: 300px;
-width: 600px;
-margin-left: 25%;
-margin-top: 5%;
-align-item: center;
-`;
-
-const Instruction = styled.div`
-position: absolute;
-z-index: 1;
-margin-left: 25%;
-height: 50%;
-width: 50%;
-margin-top:25%;
-`;
