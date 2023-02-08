8b0f7812669df65b6eb22ae868f7008eb437f074
Bianca Fu <0509biancafu@gmail.com>
2023-02-08 15:28:10 -0700

    updated createNewUser (deleted characters)

--- server/db/queries/users.js
+++ server/db/queries/users.js
@@ -2,7 +2,7 @@ const db = require('../connection');
 
 const createNewUser = function(user) {
   return db.
-  query(`INSERT INTO users (username, email, password, character) VALUES ($1, $2, $3 , $4) RETURNING *;`, [user.username, user.email, user.password, user.character])
+  query(`INSERT INTO users (username, email, password) VALUES ($1, $2, $3 , $4) RETURNING *;`, [user.username, user.email, user.password])
   .then(result => result.rows[0])
   .catch(err => console.log(err));
 }
