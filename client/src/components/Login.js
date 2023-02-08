894db2b3de85d7377060512e88eba5875274be23
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-02-08 03:36:07 -0700

    setting routing for login and signup form

---
+++ client/src/components/Login.js
@@ -0,0 +1,15 @@
+import Spline from "@splinetool/react-spline";
+import SplineScene from "./Signup/SplineScene";
+import LoginForm from "./Signup/LoginForm";
+const SignUp = () => {
+  return (
+      <div>
+        <LoginForm/>
+         <SplineScene />
+    
+      </div>
+  );
+};
+
+
+export default SignUp;
