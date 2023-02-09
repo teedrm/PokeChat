1e5182e0c45ae20f035684ccf2937e94cc7ec877
Bianca Fu <0509biancafu@gmail.com>
2023-02-09 02:29:52 +0000

    implemented settings for navbar

---
+++ client/src/components/Settings.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./settings.css";
+
+export default function Settings() {
+  return (
+    <container className="settings-container">
+      <div>This is settings</div>
+    </container>
+  );
+}
