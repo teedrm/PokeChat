1953f3ec2126dd8a2c5c7d49e443eac00c41bb00
John Kang <hbkang02@gmail.com>
2023-02-08 16:33:48 -0800

    changed app component to match master

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,7 +6,6 @@ export default function App() {
   return (
     <Wrapper className="App">
       {/* <PlayButton /> */}
-      {/* <Spline scene="https://prod.spline.design/CM9gPAJZ0-8GQZ-s/scene.splinecode" /> */}
       <Spline scene="https://prod.spline.design/cwaI5825d30JGN9H/scene.splinecode" />
     </Wrapper>
   );
