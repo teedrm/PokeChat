acadcd89104032bbe0ec5092deb4b1d55b71d20f
John Kang <hbkang02@gmail.com>
2023-02-09 19:55:12 -0800

    update on modules

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./Signup/main";
 import Login from "./Login";
 import Home from "./Home";
-import LobbyPage from "./user/lobby";
-import CreateLobby from "./user/createLobby";
+import LobbyPage from "./Lobby/lobby";
+import CreateLobby from "./Lobby/createLobby";
+import CreateUser from "./Lobby/createUser";
 
 export default function App() {
 
@@ -13,15 +14,15 @@ export default function App() {
     <div>
       <Router>
         <Routes>
-          <Route path = "/" element={<Home/>} />
+          <Route path ="/" element={<Home/>} />
           <Route path ="/signup" element={<SignUp/>} />
           <Route path ="/login" element={<Login/>} />
           <Route path ="/lobby" element={<LobbyPage />} />
           <Route path ="/CreLobby" element={<CreateLobby />} />
+          <Route path ="/selectAva" element={<CreateUser />} />
         </Routes>
       </Router>
 
     </div>
   );
 }
-
