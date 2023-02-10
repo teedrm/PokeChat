305a0511a2fe89fd8eea152e0d21b00cb2c8235d
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-02-10 00:05:45 -0700

    delete path

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,8 +7,6 @@ import LobbyPage from "./Lobby/lobby";
 import CreateLobby from "./Lobby/createLobby";
 import CreateUser from "./Lobby/createUser";
 import ChatRoom from "./ChatRoom";
-import LobbyPage from "./Lobby/lobby";
-import CreateLobby from "./Lobby/createLobby";
 
 
 export default function App() {
