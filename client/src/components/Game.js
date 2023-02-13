4041afc91ba50d2d623e95275f9b11c2bfe8b6a2
Bianca Fu <0509biancafu@gmail.com>
2023-02-12 23:07:00 -0700

    created a game route and game component

---
+++ client/src/components/Game.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Iframe from 'react-iframe';
+
+export default function Game() {
+    return <Iframe url="https://teedrm.github.io/musicplayer/"
+    width="640px"
+    height="320px"
+    id=""
+    className=""
+    display="block"
+    position="relative"/>
+}
