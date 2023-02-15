0e364a6b6138691922ceb7715cdb51f52a10970a
John Kang <hbkang02@gmail.com>
2023-02-15 01:10:01 -0800

    tetris stage size adjustment

--- client/src/components/poke-Tetris/App.js
+++ client/src/components/poke-Tetris/App.js
@@ -1,28 +1,3 @@
-// import React from "react";
-// import Tetris from "./components/Tetris";
-// import Spline from "@splinetool/react-spline";
-// import styled from "styled-components";
-// import TetrisSpline from "./components/splineTetris";
-
-// export default function App() {
-//   return (
-//     <div className="App">
-//       <TetrisSpline />
-//     </div>
-//   );
-// }
-
-// // const Wrapper = styled.div`
-// //   position: relative;
-// // `;
-
-// // const GameboyStyle = styled.div`
-// //   position: absolute;
-// //   top: 50px;
-// //   left: 50px;
-// //   z-index: 1; /* make sure the Tetris component is on top */
-// // `;
-
 import React, { useEffect, useState } from "react";
 import Tetris from "./components/Tetris";
 import Spline from "@splinetool/react-spline";
