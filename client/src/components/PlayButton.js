8ecbe6bf252ee7a046dc12f7eb1822881c1732d2
Thy Nguyen <thy.hn.nguyen@gmail.com>
2023-02-15 02:13:30 -0700

    updated navigate for play btn

--- client/src/components/PlayButton.js
+++ client/src/components/PlayButton.js
@@ -1,7 +1,9 @@
 import Spline from '@splinetool/react-spline';
+import { useNavigate } from "react-router-dom";
 
 export default function App() {
+  let navigate = useNavigate();
   return (
-    <Spline scene="https://prod.spline.design/YKcnNintwgr6h7pn/scene.splinecode" onClick={()=> window.open("/login")}/>
+    <Spline scene="https://prod.spline.design/YKcnNintwgr6h7pn/scene.splinecode"  onClick={() => navigate("/login")}/>
   );
 }
